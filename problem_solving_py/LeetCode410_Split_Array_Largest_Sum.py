43f5013a9c76f0b971dbbba086bc20aa7f70c844
Leoyin <leoyin810@gmail.com>
2022-03-31 12:47:34 +0800

    20220331

---
+++ problem_solving_py/LeetCode410_Split_Array_Largest_Sum.py
@@ -0,0 +1,47 @@
+# https://leetcode.com/problems/search-a-2d-matrix/
+
+from typing import List
+
+
+class Solution:
+    def splitArray(self, nums: List[int], m: int) -> int:
+
+        def min_subarrays_required(max_sum_allowed: int) -> int:
+            current_sum = 0
+            splits_required = 0
+
+            for element in nums:
+                # Add element only if the sum doesn't exceed max_sum_allowed
+                if current_sum + element <= max_sum_allowed:
+                    current_sum += element
+                else:
+                    # If the element addition makes sum more than max_sum_allowed
+                    # Increment the splits required and reset sum
+                    current_sum = element
+                    splits_required += 1
+
+            # Return the number of subarrays, which is the number of splits + 1
+            return splits_required + 1
+
+        # Define the left and right boundary of binary search
+        left = max(nums)
+        right = sum(nums)
+        while left <= right:
+            # Find the mid value
+            max_sum_allowed = (left + right) // 2
+
+            # Find the minimum splits. If splits_required is less than
+            # or equal to m move towards left i.e., smaller values
+            if min_subarrays_required(max_sum_allowed) <= m:
+                right = max_sum_allowed - 1
+                minimum_largest_split_sum = max_sum_allowed
+            else:
+                # Move towards right if splits_required is more than m
+                left = max_sum_allowed + 1
+
+        return minimum_largest_split_sum
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.splitArray([7,2,5,10,8,12,2,3], 3))
