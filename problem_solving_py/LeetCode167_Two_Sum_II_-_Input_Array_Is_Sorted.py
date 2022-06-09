10857e15cf31c4c9cc0460e752733697ef69456d
Leo Lee <leo.lee@varmeego.com>
2022-06-09 14:45:11 +0800

    20220609

---
+++ problem_solving_py/LeetCode167_Two_Sum_II_-_Input_Array_Is_Sorted.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers)-1
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                return [l+1, r+1]
+            elif s < target:
+                l += 1
+            else:
+                r -= 1
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.twoSum([0,0,3,4], 0))
