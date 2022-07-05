7ef1278cdc73685f6a52dd73510d10bff8c98e4e
leoyin <leoyin810@gmail.com>
2022-07-05 23:18:54 +0800

    20220705

--- problem_solving_py/LeetCode128_Longest_Consecutive_Sequence.py
+++ problem_solving_py/LeetCode128_Longest_Consecutive_Sequence.py
@@ -5,22 +5,25 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) < 2: return len(nums)
         stack = {}
         for num in nums:
             stack[num] = 1
         i, ans = 0, 0
         while i < len(nums):
-            if nums[i] + 1 in stack or nums[i] - 1 in stack:
+            if nums[i] in stack:
                 ans = max(ans, self.unionFind(nums[i]-1, stack, -1) + 1 + self.unionFind(nums[i]+1, stack, 1))
             i += 1
+        return ans
 
 
     def unionFind(self, curr, stack: dict, dir):
-        if stack[curr] is None:
+        if stack.get(curr) is None:
             return 0
-        return 1 + self.unionFind(stack[curr+dir], stack, dir)
+        stack.pop(curr)
+        return 1 + self.unionFind(curr + dir, stack, dir)
 
 
 if __name__ == "__main__":
     solution = Solution()
-    print(solution.longestConsecutive([100,4,200,1,3,2]))
+    print(solution.longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
