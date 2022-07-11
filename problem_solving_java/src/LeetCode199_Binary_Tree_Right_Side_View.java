b82d16082829e06363340aa0527e57c6940281ad
leoyin <leoyin810@gmail.com>
2022-07-11 22:44:05 +0800

    temp

---
+++ problem_solving_java/src/LeetCode199_Binary_Tree_Right_Side_View.java
@@ -0,0 +1,40 @@
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class LeetCode199_Binary_Tree_Right_Side_View {
+
+    public static void main(String[] args) {
+        LeetCode199_Binary_Tree_Right_Side_View_Solution solution = new LeetCode199_Binary_Tree_Right_Side_View_Solution();
+        TreeNode node = new TreeNode(1, new TreeNode(2, new TreeNode(5), null), new TreeNode(3, null, new TreeNode(4)));
+        System.out.println(solution.rightSideView(node));
+    }
+
+}
+
+// Not Yet Done!!!!!!
+class LeetCode199_Binary_Tree_Right_Side_View_Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        while (!Objects.isNull(root)) {
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ problem_solving_java/src/LeetCode97_Interleaving_String.java
@@ -0,0 +1,15 @@
+public class LeetCode97_Interleaving_String {
+
+    public static void main(String[] args) {
+        LeetCode97_Interleaving_String_Solution solution = new LeetCode97_Interleaving_String_Solution();
+        System.out.println(solution.isInterleave("aabcc", "dbbca", "aadbbcbcac"));
+    }
+
+}
+
+class LeetCode97_Interleaving_String_Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        return true;
+        // Not Done Yet!!!
+    }
+}
