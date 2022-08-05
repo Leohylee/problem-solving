3804eb28e9d43ad4a4bcb921711438ddba14ef30
leoyin <leoyin810@gmail.com>
2022-08-05 14:01:19 +0800

    20220805

---
+++ problem_solving_java/src/LeetCode377_Combination_Sum_IV.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class LeetCode377_Combination_Sum_IV {
+
+    public static void main(String[] args) {
+        LeetCode377_Combination_Sum_IV_Solution solution = new LeetCode377_Combination_Sum_IV_Solution();
+        System.out.println(solution.combinationSum4(new int[]{1,2,3}, 4));
+    }
+
+}
+
+class LeetCode377_Combination_Sum_IV_Solution {
+    private int[] dp;
+
+    public int combinationSum4(int[] nums, int target) {
+        dp = new int[target + 1];
+        Arrays.fill(dp, -1);
+        dp[0] = 1;
+        return helper(nums, target);
+    }
+
+    private int helper(int[] nums, int target) {
+        if (dp[target] != -1) {
+            return dp[target];
+        }
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (target >= nums[i]) {
+                res += helper(nums, target - nums[i]);
+            }
+        }
+        dp[target] = res;
+        return res;
+    }
+}
---
+++ problem_solving_java/src/LeetCode916_Word_Subsets.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode916_Word_Subsets {
+
+    public static void main(String[] args) {
+        LeetCode916_Word_Subsets_Solution solution = new LeetCode916_Word_Subsets_Solution();
+        System.out.println(solution.wordSubsets(new String[]{"amazon","apple","facebook","google","leetcode"}, new String[]{"e","o"}));
+    }
+
+}
+
+class LeetCode916_Word_Subsets_Solution {
+    public List<String> wordSubsets(String[] A, String[] B) {
+        int[] count = new int[26], tmp;
+        int i;
+        for (String b : B) {
+            tmp = counter(b);
+            for (i = 0; i < 26; ++i)
+                count[i] = Math.max(count[i], tmp[i]);
+        }
+        List<String> res = new ArrayList<>();
+        for (String a : A) {
+            tmp = counter(a);
+            for (i = 0; i < 26; ++i)
+                if (tmp[i] < count[i])
+                    break;
+            if (i == 26) res.add(a);
+        }
+        return res;
+    }
+
+    int[] counter(String word) {
+        int[] count = new int[26];
+        for (char c : word.toCharArray()) count[c - 'a']++;
+        return count;
+    }
+}
